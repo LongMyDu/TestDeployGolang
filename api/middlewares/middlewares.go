9621c03db384f81294da3796f629ff45f2caf77f
LongMyDu <longmydu@gmail.com>
2023-10-15 01:56:39 +0700

    Try to fix again

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -12,6 +12,14 @@ import (
 // SetMiddlewareJSON is...
 func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
 	return func(res http.ResponseWriter, req *http.Request) {
+		if req.Method == "OPTIONS" {
+			// Handle preflight request
+			res.Header().Set("Access-Control-Allow-Origin", "*")
+			res.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			res.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			res.Header().Set("Access-Control-Max-Age", "86400")
+			res.WriteHeader(http.StatusNoContent)
+		}
 		res.Header().Set("Content-Type", "application/json")
 
 		next(res, req)
